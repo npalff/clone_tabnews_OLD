fab29f2113b46d954c89ec2ebcc89924e46c2e9a
Nicolau Pereira Alff <nicolau.alff@gmail.com>
2026-03-03 14:49:01 +0000

    First Page - Index

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/* Index . */
+
+// React convention for components (first letter capitalized)
+function Home() {
+    return <div><h1>Index</h1><h3>Hello World! <br /> Teste</h3></div>
+
+}
+
+// We need to export the functions to render the page
+
+// Default - O padrão pra a partir dele se chama o restante da página
+export default Home;
